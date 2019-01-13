898f6182b9d05cc97393dc8a36a617ee291d6c06
Lorenz Gerber <gerberlo@gis.a-star.edu.sg>
2019-01-13 16:28:09 +0800

    adapting for version 1 data setup

--- load_data.sh
+++ load_data.sh
@@ -4,33 +4,36 @@ set -e
 echo "extracting data"
 tar xvf sg10k.tar.gz
 
+
+echo "load dataset information into database"
+PGPASSWORD=r783qjkldDsiu \
+    psql -U microaccounts_dev elixir_beacon_dev <<-EOSQL
+        INSERT INTO beacon_dataset (id, stable_id, description, access_type, reference_genome, variant_cnt, call_cnt, sample_cnt)
+        VALUES (1, 'sg10k', 'variants', 'PUBLIC', 'grch37', 1, 1, 1);
+EOSQL
+
+
+
 for FILE in *.SNPs
 do
     echo "Processing $FILE file..."
-    FILE_LENGTH=$(wc -l $FILE |  cut -f1 -d' ')
-    echo $FILE_LENGTH
-    FILE_NAME="${FILE%.*}"
-    echo $FILE_NAME
-
-    PGPASSWORD=r783qjkldDsiu \
-        psql -U microaccounts_dev elixir_beacon_dev <<-EOSQL
-            INSERT INTO beacon_dataset
-            (id, description, access_type, reference_genome, size)
-            VALUES
-            ('$FILE_NAME', 'dataset_description', 'PUBLIC', 'grch37', $FILE_LENGTH);
-EOSQL
-
-    echo "created beacon schema..."
-
-    echo "Loading sample data..."
     cat /tmp/$FILE | \
-        PGPASSWORD=r783qjkldDsiu \
-            psql -U microaccounts_dev elixir_beacon_dev -c \
-                "COPY beacon_data_table(dataset_id,chromosome,position,alternate) FROM STDIN USING DELIMITERS ';' CSV"
-
+        PGAPASSWORD=r783qjkldDsiu \
+        psql -U microaccounts_dev elixir_beacon_dev -c \
+        "COPY beacon_data_table (dataset_id,start,chromosome,reference,alternate,\"end\","type",sv_length,variant_cnt,call_cnt,sample_cnt, frequency) FROM STDIN USING DELIMITERS ';' CSV" elixir_beacon_dev
     echo "loaded sample data."
-
     rm /tmp/$FILE
-
 done
 
+PGPASSWORD=r783qkldDsiu \
+    psql -U microaccounts_dev elixir_beacon_dev <<-EOSQL
+        UPDATE beacon_dataset_table SET variant_cnt =
+        (SELECT dataset_id, count(*) FROM beacon_data_table GROUP BY dataset_id)
+EOSQL
+
+
+PGPASSWORD=r783qkldDsiu \
+    psql -U microaccounts_dev elixir_beacon_dev <<-EOSQL
+        UPDATE beacon_dataset_table SET call_cnt =
+        (SELECT dataset_id, sum(call_cnt) FROM beacon_data_table GROUP BY dataset_id)
+EOSQL
